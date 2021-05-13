8f4b0d7b95f0b31ff4502e0f9f7d1fa6a86b2c7d
WangTao <yyhz4iizf>
2021-05-13 23:13:12 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+/**
+ * @author wt
+ * @version 1.0
+ * @date 2021/5/13 22:52
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("你好世界!");
+    }
+}
